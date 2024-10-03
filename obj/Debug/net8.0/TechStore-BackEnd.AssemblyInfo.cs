18b04ef3e0fc260d9cc5a49bcfdc6a0938e2eec5
The-G-Man-Half-Life <mateomoospina@gmail.com>
2024-10-03 10:14:43 -0500

    feat: creation of the er diagram

--- obj/Debug/net8.0/TechStore-BackEnd.AssemblyInfo.cs
+++ obj/Debug/net8.0/TechStore-BackEnd.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TechStore-BackEnd")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5757b64d12c795ec77c12d0f3706cd5fa662dba3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+41e248a31379ef6188cb1ba13949a1853642324f")]
 [assembly: System.Reflection.AssemblyProductAttribute("TechStore-BackEnd")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TechStore-BackEnd")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
