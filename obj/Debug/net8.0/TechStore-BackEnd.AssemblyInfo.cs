41e248a31379ef6188cb1ba13949a1853642324f
The-G-Man-Half-Life <mateomoospina@gmail.com>
2024-10-03 08:14:04 -0500

    feat: creation of the diagram folder

--- obj/Debug/net8.0/TechStore-BackEnd.AssemblyInfo.cs
+++ obj/Debug/net8.0/TechStore-BackEnd.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TechStore-BackEnd")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9612a9448e71d8ca0638f23fb5df30cc7f5b709a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5757b64d12c795ec77c12d0f3706cd5fa662dba3")]
 [assembly: System.Reflection.AssemblyProductAttribute("TechStore-BackEnd")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TechStore-BackEnd")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
