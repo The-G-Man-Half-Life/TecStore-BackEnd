bc0cb3b8442a47229d0b9db11710531904ed1a04
The-G-Man-Half-Life <mateomoospina@gmail.com>
2024-10-06 17:23:57 -0500

    feat: created the brand get controller

---
+++ Controllers/v1/Brands/BrandGetController.cs
@@ -0,0 +1,58 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TechStore_BackEnd.Models;
+using TechStore_BackEnd.Services;
+
+namespace TechStore_BackEnd.Controllers.v1.Brands;
+    
+[ApiController]
+[Route("api/v1/Brands/[controller]")]
+[ApiExplorerSettings(GroupName = "v1")]
+[Tags ("Brands")]
+public class BrandGetController : ControllerBase
+{
+    public readonly BrandServices BrandServices;
+
+    public BrandGetController(BrandServices BrandServices)
+    {
+        this.BrandServices = BrandServices;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<Brand>>> GetAllbrands()
+    {
+        try
+        {
+            var brands = await BrandServices.GetAll();
+            return Ok(brands);
+        }
+        catch (DbUpdateException dbEX)
+        {
+            
+            throw new DbUpdateException($"Un error ocurrio durante el proceso {dbEX.Message}");
+        }
+    }
+
+    [HttpGet("{id}")]
+    public async Task<ActionResult<Brand>> GetById([FromRoute] int id)
+    {
+        if(await BrandServices.CheckExistence(id) == false)
+        {
+            return NoContent();
+        }
+        else
+        {
+            try
+            {
+                var brandFound = await BrandServices.GetById(id);
+                return Ok(brandFound);          
+            }
+            catch (DbUpdateException dbEX)
+            {
+                
+                throw new DbUpdateException("Un error ocurrio durante el proceso", dbEX);
+            }
+
+        }
+    }
+}
--- obj/Debug/net8.0/TechStore-BackEnd.AssemblyInfo.cs
+++ obj/Debug/net8.0/TechStore-BackEnd.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TechStore-BackEnd")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+496672a5489ada0af19bab752ab082396f2bd899")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+35b303dadded34c94ef8564cbe26d3ea4952f9c9")]
 [assembly: System.Reflection.AssemblyProductAttribute("TechStore-BackEnd")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TechStore-BackEnd")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
